0cdf7f7bc39b3d73a1cabdd1d6469fdedfbd96bc
LucasAurelio0 <zeaureliosv@gmail.com>
2024-10-14 20:20:34 -0300

    fix: CORS

--- src/main/java/me/dio/santander_dev_week_2023/Application.java
+++ src/main/java/me/dio/santander_dev_week_2023/Application.java
@@ -2,7 +2,12 @@ package me.dio.santander_dev_week_2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Description;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.*;
+
+@OpenAPIDefinition(servers={@Server(url="/")})
 @SpringBootApplication
 public class Application {
 
